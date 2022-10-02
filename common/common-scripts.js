c6e945c75976cb58c2753628a74a3d1a3d711e2e
Bananafoolish <100505456+Bananafoolish@users.noreply.github.com>
2022-10-02 17:33:38 +0900

    Update common-scripts.js

--- common/common-scripts.js
+++ common/common-scripts.js
@@ -1,33 +1,47 @@
 let main = document.getElementById("main");
 let article = document.getElementById("article");
-let commonout = '<div id="menushadow" class="sidemenu_off"></div><div id="sidemenu" class="sidemenu_off"><h2>クレジット</h2><p><a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans Japanese</a> by Google</p><h2>リンク</h2><p><a href="/index.html">トップページ</a></p><p><a href="/article/index.html">記事一覧</a></p></div><header><label class="hamburger" id="hamburger1"><div></div><div></div><div></div><input type="checkbox" id="sideswitch" onclick="slideClick()" for="hamburger1"></label></header>'
-let commoninner ='<footer><p>特記のない限り、ページ内のコンテンツは全て<a href="http://creativecommons.org/licenses/by-sa/2.0/"> CC BY-SA 2.0</a>の下に利用可能です。</p></footer>';
-let commonads = '<label id="forads">広告を見る<input type="button" onClick="adsClick()" for="forads"></label>';
-let adslist = ["541060657"];
-main.insertAdjacentHTML("beforeend", commoninner);
-main.insertAdjacentHTML("beforebegin", commonout);
-article.insertAdjacentHTML("beforeend", commonads);
+let common_1 = '<div id="menu_shadow" class="left_off"></div><div class="left_off" id="left_menu"><h2>クレジット</h2><p><a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans Japanese</a> by Google</p><h2>リンク</h2><p><a href="/index.html">トップページ</a></p><p><a href="/article/index.html">記事一覧</a></p></div><header><label class="hamburger" id="hamburger_left"><div></div><div></div><div></div><input type="checkbox" id="left_switch" onclick="left_Click()" for="hamburger_left"></label><label class="hamburger" id="hamburger_right"><div></div><div></div><div></div><input type="checkbox" id="right_switch" onclick="right_Click()" for="hamburger_right"></label></header>'
+let common_2 = '<label id="for_ads">広告を見る<input type="button" onClick="ads_Click()" for="for_ads"></label>';
+let common_3 = '<div class="right_off" id="right_menu"><h2>サイド</h2></div>';
+let common_4 = '<footer><p>特記のない限り、ページ内のコンテンツは全て<a href="http://creativecommons.org/licenses/by-sa/2.0/"> CC BY-SA 2.0</a>の下に利用可能です。</p></footer>';
+main.insertAdjacentHTML("afterend", common_1);
+article.insertAdjacentHTML("beforeend", common_2);
+article.insertAdjacentHTML("afterend", common_3);
+main.insertAdjacentHTML("beforeend", common_4);
 
-let forads = document.getElementById("forads");
+let for_ads = document.getElementById("for_ads");
 
-let sideswitch = document.getElementById("sideswitch");
-sideswitch.checked = true;
-let sidemenu = document.getElementById("sidemenu");
-let menushadow = document.getElementById("menushadow");
+let left_switch = document.getElementById("left_switch");
+left_switch.checked = true;
+let left_menu = document.getElementById("left_menu");
+let menu_shadow = document.getElementById("menu_shadow");
 
-let adshtml = '<iframe src="https://turbowarp.org/' + adslist[Math.floor(Math.random() * adslist.length)] + '/embed/?interpolate&hqpen" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>';
+let right_switch = document.getElementById("right_switch");
+let right_menu = document.getElementById("right_menu");
 
-function slideClick() {
-	if (sideswitch.checked) {
-		sidemenu.classList.add("sidemenu_off");
-		menushadow.classList.add("sidemenu_off");
+let ads_list = ["541060657"];
+let ads_random = Math.floor(Math.random() * ads_list.length);
+let ads_html = '<iframe src="https://turbowarp.org/' + ads_list[ads_random] + '/embed/?interpolate&hqpen" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe><a href ="https://scratch.mit.edu/projects/' + ads_list[ads_random] + '">リンク</a>';
+
+function left_Click() {
+	if (left_switch.checked) {
+		left_menu.classList.add("left_off");
+		menu_shadow.classList.add("left_off");
 	} else {
-		sidemenu.classList.remove("sidemenu_off");
-		menushadow.classList.remove("sidemenu_off");
+		left_menu.classList.remove("left_off");
+		menu_shadow.classList.remove("left_off");
 	}
 }
 
-function adsClick() {
-	forads.innerHTML = adshtml;
+function right_Click() {
+	if (right_switch.checked) {
+		right_menu.classList.add("right_off");
+	} else {
+		right_menu.classList.remove("right_off");
+	}
+}
+
+function ads_Click() {
+	for_ads.innerHTML = ads_html;
 }
 
