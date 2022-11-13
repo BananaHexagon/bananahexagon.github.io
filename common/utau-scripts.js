dfab33194e00eb76a1a3791e1ca59d80566f2d5f
Bananafoolish <100505456+Bananafoolish@users.noreply.github.com>
2022-11-13 19:49:54 +0900

    Update utau-scripts.js

--- common/utau-scripts.js
+++ common/utau-scripts.js
@@ -1,3 +1,2 @@
-const left_menu = document.getElementById("left_menu");
 const common_3 = '<p><a href="https://undertale.com">Undertale</a> by TobyFox (非<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA 2.0</a>)</p>';
-left_menu.insertAdjacentHTML("afterend", common_3);
+left_menu.insertAdjacentHTML("beforeend", common_3);
