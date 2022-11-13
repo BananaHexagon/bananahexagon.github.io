02ac08af9ff8a1f89b5a1acf6b9449d23e5810f3
Bananafoolish <100505456+Bananafoolish@users.noreply.github.com>
2022-11-13 20:29:08 +0900

    Update utau-scripts.js

--- common/utau-scripts.js
+++ common/utau-scripts.js
@@ -1,7 +1,7 @@
 const utau_1 ='<p><a href="/creation/sludgetale/pages">記事一覧</a></p>';
-const left_menu_credits = document.getElementById(left_menu_credits);
+const left_menu_credits = document.getElementById("left_menu_credits");
 left_menu_links.insertAdjacentHTML("beforeend", utau_1);
 
 const utau_2 = '<p><a href="https://undertale.com">Undertale</a> by TobyFox (非<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA 2.0</a>)</p>';
-const left_menu_links = document.getElementById(left_menu_links);
+const left_menu_links = document.getElementById("left_menu_links");
 left_menu_links.insertAdjacentHTML("beforeend", utau_2);
