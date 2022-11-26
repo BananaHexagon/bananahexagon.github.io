b4e9cf739c1cdc3dca236daee6f99ffe287fa101
Bananafoolish <100505456+Bananafoolish@users.noreply.github.com>
2022-11-26 13:02:34 +0900

    Update common-scripts.js

--- common/common-scripts.js
+++ common/common-scripts.js
@@ -4,7 +4,11 @@ const obj = JSON.parse(json);
 const main = document.getElementById("main");
 const article = document.getElementById("article");
 const credits = document.getElementById("page_credits");
-const common_1 = '<div id="menu_shadow" class="left_off"></div><div class="left_off" id="left_menu"><div id="left_menu_links"><h2>リンク</h2><p><a href="/index.html">トップページ</a></p><p><a href="/article/">ブログ</a></p><p><a href="/creations/sludgetale/">Sludgetale</a></p><p><a href="https://discord.gg/3JZvH8fqzJ">Discordサーバー(外部リンク)</a></p></div><div id="left_menu_credits"><h2>クレジット</h2><p><a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans Japanese</a> by Google</p>' + credits.innerHTML + '</div><header><label class="hamburger" id="hamburger_left"><div></div><div></div><div></div><input type="checkbox" id="left_switch" onclick="left_Click()" for="hamburger_left"></label></header>'
+var credits_i = credits.innerHTML;
+if (credits == null) {
+	credits_i = "";
+}
+const common_1 = '<div id="menu_shadow" class="left_off"></div><div class="left_off" id="left_menu"><div id="left_menu_links"><h2>リンク</h2><p><a href="/index.html">トップページ</a></p><p><a href="/article/">ブログ</a></p><p><a href="/creations/sludgetale/">Sludgetale</a></p><p><a href="https://discord.gg/3JZvH8fqzJ">Discordサーバー(外部リンク)</a></p></div><div id="left_menu_credits"><h2>クレジット</h2><p><a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans Japanese</a> by Google</p>' + credits_i + '</div><header><label class="hamburger" id="hamburger_left"><div></div><div></div><div></div><input type="checkbox" id="left_switch" onclick="left_Click()" for="hamburger_left"></label></header>'
 const common_2 = '<footer><div></div><p>特記のない限り、ページ内のコンテンツは全て<a href="http://creativecommons.org/licenses/by-sa/2.0/"> CC BY-SA 2.0</a>の下に利用可能です。</p><div></div></footer>';
 main.insertAdjacentHTML("afterend", common_1);
 main.insertAdjacentHTML("beforeend", common_2);
