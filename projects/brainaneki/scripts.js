0e25289171925f57f359c45b75a171065e21c287
Bananafoolish <100505456+Bananafoolish@users.noreply.github.com>
2022-11-26 13:15:02 +0900

    Update scripts.js

--- projects/brainaneki/scripts.js
+++ projects/brainaneki/scripts.js
@@ -1,8 +1,9 @@
 function runAneki() {
   interpret(compile(document.getElementById("code").value));
 }
+
 function compile(code) {
-	let antoken = ["！！！","？？？","とても","クール","あたしを呼んだかい！","あんた","って言ったよねぇぇぇぇ！！！！？？？？ねええぇぇぇぇぇぇぇええ！！！お姉さんあれだから！正直に言えば怒らないから！"]
+	let antoken = ["！！！","？？？","とても","クール","ばか","あんた","って言ったよねぇぇぇぇ！！！！？？？？ねええぇぇぇぇぇぇぇええ！！！お姉さんあれだから！正直に言えば怒らないから！"]
 	let bftoken = [">","<","+","-",".","[","]"]
 	let str = "";
 	let out = "";
@@ -17,7 +18,7 @@ function compile(code) {
 }
 
 function interpret(code) {
-	let ascii = "-------------------------------- !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+	let ascii = "\0------\a\b\t\n\v\f\r----------\e------- !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 	let out = "";
 	let mem = [...Array(37564)].map(x => 0);
 	let point = 18782;
@@ -74,11 +75,11 @@ function interpret(code) {
 				break;
 		}
 	}
-	alert(out);
+	console.log(out);
 }
 
 function anticompile(code) {
-	let antoken = ["！！！","？？？","とても","クール","あたしを呼んだかい！","あんた","って言ったよねぇぇぇぇ！！！！？？？？ねええぇぇぇぇぇぇぇええ！！！お姉さんあれだから！正直に言えば怒らないから！"]
+	let antoken = ["！！！","？？？","とても","クール","ばか","あんた","って言ったよねぇぇぇぇ！！！！？？？？ねええぇぇぇぇぇぇぇええ！！！お姉さんあれだから！正直に言えば怒らないから！"]
 	let bftoken = [">","<","+","-",".","[","]"]
 	let out = "";
 	for(let i = 0; i < code.length; i++) {
