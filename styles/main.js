02b5d13014e37942f5a94a4cd4a3d8ec5af9edc8
Bananahexagon <100505456+Bananahexagon@users.noreply.github.com>
2023-02-26 17:45:43 +0900

    Add files via upload

---
+++ styles/main.js
@@ -0,0 +1,57 @@
+let main = document.querySelector("main");
+main.insertAdjacentHTML("beforebegin", `
+<header>
+<a href="/index.html"><img src="/styles/bananahexagon.png"></a>
+<ul>
+  <li>Info
+    <ul>
+      <li><a href="/info/about">About</a></li>
+      <li><a href="/info/profile">Profile</a></li>
+      <li><a href="/info/contact">Contact</a></li>
+    </ul>
+  </li>
+  <li>Blog
+    <ul>
+      <li><a href="/blog/2023">2023</a></li>
+    </ul>
+  </li>
+  <li>Projects
+    <ul>
+      <li><a href="/projects/sludgetale">Sludgetale</a></li>
+      <li><a href="/projects/">Other</a></li>
+    </ul>
+  </li>
+</ul>
+<div class="menu_button">
+  <div />
+</div>
+</header>
+`);
+main.insertAdjacentHTML("beforebegin", `
+<div class="sidemenu">
+  <div class="menu_shadow" />
+  <div class="menu_content">
+    <h2>
+      クレジット
+    </h2>
+    <ul>
+      <li>
+      <p>Font : <a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans Japanese</a></p>
+      </li>
+    </ul>
+  </div>
+</div>
+`);
+main.insertAdjacentHTML("beforeend", `
+<footer>
+  <p>特記のない限り、このサイトのコンテンツは<a href="https://creativecommons.org/licenses/by-sa/4.0/deed.ja">CC BY-SA 4.0</a>の下で利用可能です</p>
+</footer>
+`);
+
+let menu_button = document.querySelector(".menu_button");    
+let side_menu = document.querySelector(".sidemenu");    
+  menu_button.addEventListener("click", () => {
+    menu_button.classList.toggle("button_right");
+    side_menu.classList.toggle("menu_opened");
+	}
+);
