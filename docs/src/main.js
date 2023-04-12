9cbe0b25d498be7e018f5b1bbdb2770029261337
Bananahexagon <100505456+Bananahexagon@users.noreply.github.com>
2023-04-12 22:04:38 +0900

    a

--- docs/src/main.js
+++ docs/src/main.js
@@ -30,7 +30,7 @@ window.onload = () => {
     time_stamp += last_edited == null ? "" : last_edited;
     const page_created = document.querySelector("meta[name='page_created']");
     time_stamp += page_created == null ? "" : page_created;
-    (document.getElementsByClassName("time_stamp")[0]).insertAdjacentText("afterbegin", time_stamp);
+    document.getElementsByClassName("time_stamp")[0].textContent(time_stamp);
     
     let menu_button = document.querySelector(".menu_button");
     let side_menu = document.querySelector(".sidemenu");
