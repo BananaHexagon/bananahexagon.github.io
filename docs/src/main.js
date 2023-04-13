c32d0dcaa1fcc6afda42b1e19ea16b1c293ede5b
Bananahexagon <100505456+Bananahexagon@users.noreply.github.com>
2023-04-13 22:09:07 +0900

    a

--- docs/src/main.js
+++ docs/src/main.js
@@ -27,9 +27,9 @@ window.onload = () => {
     Importjs("/src/styles_pc.js").onload = () => {
         let time_stamp = "";
         const page_created = document.querySelector("meta[name='page_created']");
-        time_stamp += page_created == "作成日" + (null ? "-" : page_created.content);
+        time_stamp += "作成日" + (page_created == null ? "-" : page_created.content);
         const last_edited = document.querySelector("meta[name='last_edited']");
-        time_stamp += last_edited == "更新日" + (null ? "-" : last_edited.content);
+        time_stamp += "更新日" + (last_edited == null ? "-" : last_edited.content);
         document.getElementsByClassName("time_stamp")[0].textContent = time_stamp;
 
         let menu_button = document.querySelector(".menu_button");
