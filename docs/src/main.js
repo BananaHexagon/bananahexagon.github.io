5e5058fb7e2bc203b070274680f1eb1b51e71e77
bananahexagon <obsidian.bananafool@gmail.com>
2023-07-17 19:43:13 +0900

    updated ui

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,13 +1,13 @@
 "use strict"
 
-function Importjs(src) {
+const importJS = src => {
     let element = document.createElement("script");
     element.src = src;
     document.body.appendChild(element);
     return element;
 }
 
-function Importcss(src) {
+const importCSS = src => {
     let element = document.createElement("link");
     element.rel = "stylesheet";
     element.href = src;
@@ -15,30 +15,35 @@ function Importcss(src) {
     return element;
 }
 
-Importcss("/src/styles_pc.css");
-document.head.appendChild((() => {
-    let element = document.createElement("link");
-    element.rel = "icon";
-    element.href = "/assets/favicon.ico";
-    return element;
-})())
+const createElement = async html => {
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    return div.firstElementChild;
+}
 
-window.onload = () => {
-    if (document.querySelector("meta[name='stub']") != null) {
-        window.location.href = "./404.html"
-    } else Importjs("/src/styles_pc.js").onload = () => {
-        let time_stamp = "";
+importCSS("/src/styles_pc.css");
+window.onload = async () => {
+    importJS("/src/styles_pc.js").onload = async () => {
+
+        let time_stamp_display = "";
         const page_created = document.querySelector("meta[name='page_created']");
-        time_stamp += "作成日 - " + (page_created == null ? "" : page_created.content + " ") + "| ";
+        time_stamp_display += "作成日 - " + (page_created == null ? "" : page_created.content + " ") + "| ";
         const last_edited = document.querySelector("meta[name='last_edited']");
-        time_stamp += "更新日 - " + (last_edited == null ? "" : last_edited.content);
-        document.getElementsByClassName("time_stamp")[0].textContent = time_stamp;
+        time_stamp_display += "更新日 - " + (last_edited == null ? "" : last_edited.content);
+        const time_stamp = createElement(`<div class="time_stamp">${time_stamp_display}</div>`);
+        const article = document.querySelector("article");
 
-        let menu_button = document.querySelector(".menu_button");
-        let side_menu = document.querySelector(".sidemenu");
-        menu_button.addEventListener("click", () => {
-            menu_button.classList.toggle("button_right");
-            side_menu.classList.toggle("menu_opened");
+        const body = document.body;
+        await Promise.all([header, footer, side_menu, time_stamp, article])
+        body.insertAdjacentElement("afterbegin", await side_menu);
+        body.insertAdjacentElement("beforeend", await header);
+        body.insertAdjacentElement("beforeend", await footer);
+        (await article).insertAdjacentElement("afterbegin", await time_stamp);
+
+
+        let side_button = document.querySelector(".side_button");
+        side_button.addEventListener("click", async () => {
+            (await side_menu).classList.toggle("opened");
         });
     }
 }
--- docs/src/styles_pc.js
+++ docs/src/styles_pc.js
@@ -1,60 +1,44 @@
 "use strict"
 
-let main = document.querySelector("main");
-let article = document.querySelector("article");
-
-main.insertAdjacentHTML("afterend", `
+const header = createElement(`
 <header>
-  <a href="/index.html"><img src="/assets/bananahexagon.png"></a>
-  <ul>
-    <li>Info
-      <ul>
-        <li><a href="/info/about">About</a></li>
-        <li><a href="/info/profile">Profile</a></li>
-        <li><a href="/info/contact">Contact</a></li>
-      </ul>
-    </li>
-    <li>Blog
-      <ul>
-        <li><a href="/blog/2023">2023</a></li>
-      </ul>
-    </li>
-    <li>Projects
-      <ul>
-        <li><a href="https://github.com/bananahexagon/sugot-lang">Sugot</a></li>
-        <li><a href="/sludgetale">Sludgetale</a></li>
-        <li><a href="/projects">Other</a></li>
-      </ul>
-    </li>
-  </ul>
-  <div class="menu_button">
-    <div />
-  </div>
-</header>
-`);
-
-main.insertAdjacentHTML("beforebegin", `
-<div class="sidemenu">
-  <div class="menu_shadow" />
-  <div class="menu_content">
-    <h2>
-      クレジット
-    </h2>
+    <a href="https://bananahexagon.github.io"><img src="assets/dev_icon.png"></a>
     <ul>
-      <li>
-      <p>Font : <a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans Japanese</a></p>
-      </li>
+        <li>Info
+            <ul>
+                <li><a href="/info/about">About</a></li>
+                <li><a href="/info/profile">Profile</a></li>
+                <li><a href="/info/contact">Contact</a></li>
+            </ul>
+        </li>
+        <li>Blog
+            <ul>
+                <li><a href="/blog/2023">2023</a></li>
+            </ul>
+        </li>
+        <li>Projects
+            <ul>
+                <li><a href="https://github.com/bananahexagon/sugot-lang">Sugot</a></li>
+                <li><a href="/projects">Other</a></li>
+            </ul>
+        </li>
     </ul>
-  </div>
-</div>
-`);
+    <img src="assets/hamburger_black.svg" class="side_button">
+</header>
+`)
 
-main.insertAdjacentHTML("beforeend", `
+const footer = createElement(`
 <footer>
-  <p>特記のない限り、このサイトのコンテンツは<a href="https://creativecommons.org/licenses/by-sa/4.0/deed.ja">CC BY-SA 4.0</a>の下で利用可能です。</p>
+    <p>特記のない限り、このサイトのコンテンツは<a href="https://creativecommons.org/licenses/by-sa/4.0/deed.ja">CC BY-SA 4.0</a>の下で利用可能です。</p>
 </footer>
-`);
+`)
 
-article.insertAdjacentHTML("afterbegin",`
-<div class="time_stamp">​</div>
-`);
+let side_menu = createElement(`
+<div class="side_menu">
+    <div class="side_menu_shadow"></div>
+    <div class="side_menu_content">
+        <h1>クレジット</h1>
+        <p><a href="https://fonts.google.com/noto/specimen/Noto+Sans+JP">Noto Sans JP</a> by Google</p>
+    </div>
+</div>
+`)
